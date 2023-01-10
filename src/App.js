37e4e3ff6e1600d9a95da1e68ac8f624aef9cff9
TeeCreates <99111124+TeeCreates@users.noreply.github.com>
2023-01-09 22:36:36 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { Home } from "./Home";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Navbar from "./Navbar";
+
+import Navbar from "./Navbar";
+import { MyWork } from "./Work";
+import { About } from "./About";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/work">
+              <MyWork />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
+// const Body = styled.div`
+//   background-color: var(--peach);
+// `;
