f95e673337265f82171955110750ef079e42ed51
TeeCreates <99111124+TeeCreates@users.noreply.github.com>
2023-03-25 00:54:46 -0400

    final

--- src/Work.js
+++ src/Work.js
@@ -46,7 +46,7 @@ export const MyWork = () => {
 };
 
 const ProjectWrapper = styled.div`
-  width: 50%;
+  width: 30%;
   background-color: white;
   height: 120px;
   border-radius: 15px;
@@ -68,7 +68,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   height: 100%;
-  @media (max-width: 700px) {
+  @media (max-width: 800px) {
     height: 100vh;
   }
 `;
@@ -89,7 +89,7 @@ const BigLink = styled.a`
   color: #7847e3;
   font-size: 20px;
   display: flex;
-  @media (max-width: 700px) {
+  @media (max-width: 800px) {
     display: none;
   }
 `;
@@ -110,7 +110,7 @@ const SmallLink = styled.a`
   font-size: 20px;
   display: flex;
   position: relative;
-  @media (min-width: 700px) {
+  @media (min-width: 800px) {
     display: none;
   }
 `;
